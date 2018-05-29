39d4ae3a867a093315d3c581fff571f6cbdb36b1
Simon de Lang <simondelang@gmail.com>
2018-05-29 09:19:45 +0200

    Use SonarQube API to get quality gate status
    Does not work with self signed certificates

--- tasks/SonarBuildbreaker/task.ts
+++ tasks/SonarBuildbreaker/task.ts
@@ -1,17 +1,57 @@
 import * as tl from 'vsts-task-lib/task';
 import * as path from 'path';
 import * as fs from 'fs-extra';
+import * as request from 'request';
 
 async function run() {
-  const sqResultPath = path.join(tl.getVariable('Agent.BuildDirectory'), 'sonarqube-result.json');
-  await fs.readFile(sqResultPath, 'UTF-8').then(result => {
-    const content = JSON.parse(result);
-    if (content.status === 'ERROR') {
-      tl.setResult(tl.TaskResult.Failed, `The quality gate has failed with status: ${content.status}`);
-    } else {
-      console.log(`The quality gate passed with status: ${content.status}`)
-    }
+  getTaskReport().then(report => {
+    request(`${report.serverUrl}/api/ce/task?id=${report.ceTaskId}`, (error, response, body) => {
+      request(`${report.serverUrl}/api/qualitygates/project_status?analysisId=${JSON.parse(body).task.analysisId}`, (error, response, body) => {
+        const projectStatus = JSON.parse(body).projectStatus;
+        if (projectStatus.status === 'ERROR') {
+          tl.setResult(tl.TaskResult.Failed, `The quality gate has failed with status: ${projectStatus.status}`);
+        } else {
+          console.log(`The quality gate passed with status: ${projectStatus.status}`)
+        }
+      });
+    });
   });
 }
+
+async function getTaskReport(): Promise<TaskResult> {
+  return new Promise<TaskResult>(resolve => {
+    const taskReportGlob = path.join('**', 'report-task.txt');
+    const taskReportGlobResult = tl.findMatch(
+      tl.getVariable('Agent.BuildDirectory'),
+      taskReportGlob
+    );
+    fs.readFile(taskReportGlobResult[0], 'UTF-8').then(fileContent => {
+      const lines: string[] = fileContent.replace(/\r\n/g, '\n').split('\n'); // proofs against xplat line-ending issues
+      const settings = new Map<string, string>();
+      lines.forEach((line: string) => {
+        const splitLine = line.split('=');
+        if (splitLine.length > 1) {
+          settings.set(splitLine[0], splitLine.slice(1, splitLine.length).join());
+        }
+      });
+
+      resolve({
+        ceTaskId: settings.get('ceTaskId'),
+        ceTaskUrl: settings.get('ceTaskUrl'),
+        dashboardUrl: settings.get('dashboardUrl'),
+        projectKey: settings.get('projectKey'),
+        serverUrl: settings.get('serverUrl')
+      });
+    });
+  });
+}
+
+interface TaskResult {
+  ceTaskId: string,
+  ceTaskUrl: string,
+  dashboardUrl: string,
+  projectKey: string,
+  serverUrl: string,
+};
 
 run();
