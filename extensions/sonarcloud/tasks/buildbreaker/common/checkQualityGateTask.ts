b612b4607e090cbf8f195fe70dcc08ae5ec32392
Simon de Lang <simondelang@gmail.com>
2018-07-11 16:40:11 +0200

    Improve logging
    sonar-buildbreaker@7.0.3
    sonarcloud-buildbreaker@1.0.5

--- extensions/sonarcloud/tasks/buildbreaker/common/checkQualityGateTask.ts
+++ extensions/sonarcloud/tasks/buildbreaker/common/checkQualityGateTask.ts
@@ -8,14 +8,21 @@ import Endpoint from './Endpoint';
 export default async function checkQualityGateTask(endpoint: Endpoint) {
   return new Promise<void>(resolve => {
     getTaskReport().then(report => {
+      console.log(`Retrieving analysisId from ${report.serverUrl}/api/ce/task?id=${report.ceTaskId}`);
       request.get(
         {
           method: 'GET',
           baseUrl: report.serverUrl,
           uri: `/api/ce/task?id=${report.ceTaskId}`,
           json: true,
-          auth: endpoint.auth
+          auth: endpoint.auth,
+
         }, (error, response, body) => {
+          handleErrors(error, response);
+          if (!body.task || !body.task.analysisId) {
+            tl.setResult(tl.TaskResult.Failed, `Unknown result from previous http request. The body is: ${body}. Statuscode is ${response.statusCode}`);
+          }
+          console.log(`Retrieving quality gate report from ${report.serverUrl}/api/qualitygates/project_status?analysisId=${body.task.analysisId}`);
           request.get(
             {
               method: 'GET',
@@ -24,11 +31,16 @@ export default async function checkQualityGateTask(endpoint: Endpoint) {
               json: true,
               auth: endpoint.auth
             }, (error, response, body) => {
+              handleErrors(error, response);
+              if (!body.projectStatus || !body.projectStatus.status) {
+                tl.setResult(tl.TaskResult.Failed, `Unknown result from previous http request. The body is: ${body}. Statuscode is ${response.statusCode}`);
+              }
               const projectStatus = body.projectStatus;
+              console.log(`The quality gate retrieved. The status of the quality gate is ${projectStatus.status}`);
               if (projectStatus.status === 'ERROR') {
-                tl.setResult(tl.TaskResult.Failed, `The quality gate has failed with status: ${projectStatus.status}`);
+                tl.setResult(tl.TaskResult.Failed, `The analysis did not pass the quality gate because it has the status: ${projectStatus.status}. Attempting to fail the build!`);
               } else {
-                console.log(`The quality gate passed with status: ${projectStatus.status}`)
+                console.log(`The analysis has passed the quality gate: ${projectStatus.status}`);
               }
               resolve();
             });
@@ -44,6 +56,7 @@ async function getTaskReport(): Promise<TaskResult> {
       tl.getVariable('Agent.BuildDirectory'),
       taskReportGlob
     );
+    console.log(`Getting task report from file ${taskReportGlobResult[0]}`);
     fs.readFile(taskReportGlobResult[0], 'UTF-8').then(fileContent => {
       const lines: string[] = fileContent.replace(/\r\n/g, '\n').split('\n'); // proofs against xplat line-ending issues
       const settings = new Map<string, string>();
@@ -64,3 +77,9 @@ async function getTaskReport(): Promise<TaskResult> {
     });
   });
 };
+
+function handleErrors(error: any, response: request.Response) {
+  if (response.statusCode < 200 || response.statusCode >= 300) {
+    tl.setResult(tl.TaskResult.Failed, `Something went wrong! Got statuscode ${response.statusCode} and error ${error}`);
+  }
+}
